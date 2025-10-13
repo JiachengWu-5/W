ec1841a6c863af981a7d9d4df00f7a8b48185984
JiachengWU <65650202+JiachengWu-5@users.noreply.github.com>
2025-10-13 08:26:12 +0800

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jiachengwu-5.github.io/W",
-	base: "/",
+	site: "https://jiachengwu-5.github.io",
+	base: "/W",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
